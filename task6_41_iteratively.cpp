9da7f4d4da97b04095dc6142571c1a1cd815445b
Sue T <84173055+SueT74@users.noreply.github.com>
2021-11-19 21:49:47 +0200

    Update task6_41_iteratively.cpp

--- task6_41_iteratively.cpp
+++ task6_41_iteratively.cpp
@@ -3,45 +3,44 @@
 #include <iostream>
 using namespace std;
 
-int gcd(int x, int y)
+int gcd(int,int)
 {
 	cout << "Find the greatest common divisor of two numbers - Iteratively"<< endl;
-    	cout << "*************************************************************"<< endl;
+	cout << "*************************************************************"<< endl;
+	
+	int x,y,r; 
 	
 	// get numbers from user
     	cout << "Enter x and y: ";
     	cin >> x >> y;
-	
-
-	if (x==0) 
-	{
-		return y;
-    	}
-    	else if (y==0)
-	{
-		return x;		
-	}
-
-    	while (x != y) 
-	{
-        	if (x > y)
-		{
-			x = x - y;
-		}
-
-       	 	else
-		{
-			y = y - x;
-		}       
-   	 }
     
-    	return x;
+	//make sure x > y
+	if (x < y) 
+	{
+		int tmp = x;
+		x = y;
+		y = tmp;
+   	}
+	// if y is 0 gcd = x's value
+	if (y == 0) 
+	{
+        	return x;
+   	} 
+	//work out gcd
+   	while(y != 0)
+	{
+		r = x % y;
+		x = y;
+		y = r;
+    	}
+    	
+   	return x;
 }
  
 
 int main()
 {
-	int x,y;
-    	cout<< "GCD is: " <<gcd(x,y);
-    	return 0;
+	    int x,y;
+	    cout<< "GCD is: " <<gcd(x,y);
+	    return 0;
 }
