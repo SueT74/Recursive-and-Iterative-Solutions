fdb42864db23c0ec25c832705f934293d9788e17
Sue T <84173055+SueT74@users.noreply.github.com>
2021-11-19 20:50:21 +0200

    Add files via upload

---
+++ task6_41_iteratively.cpp
@@ -0,0 +1,44 @@
+// Susan Tunkkari C++ 2021 NITS19K 
+//ch6 task 6.41 done iteratively
+#include <iostream>
+using namespace std;
+
+int gcd(int x, int y)
+{
+    cout << "Find the greatest common divisor of two numbers - Iteratively"<< endl;
+    cout << "***********************************************************"<< endl;
+	
+	// get numbers from user
+    cout << "Enter x and y: ";
+    cin >> x >> y;
+
+	 if (x < y) 
+	{
+        int tmp = x;
+        x = y;
+        y = tmp;
+    }
+
+    while (x != y) 
+	{
+        if (x > y)
+		{
+			x = x - y;
+		}
+
+        else
+		{
+			y = y - x;
+		}       
+    }
+    
+    return x;
+}
+ 
+
+int main()
+{
+	int x,y;
+    cout<< "GCD is: " <<gcd(x,y);
+    return 0;
+}
---
+++ task6_41_recursively.cpp
@@ -0,0 +1,48 @@
+/*The greatest common divisor of integers x and y is
+the largest integer that evenly divides both x and y. Write a recursive function gcd that returns the
+greatest common divisor of x and y, defined recursively as follows: If y is equal to 0, then gcd(x, y)
+is x; otherwise, gcd(x, y) is gcd(y, x % y), where % is the remainder operator. [Note: For this algorithm,
+x must be larger than y.]*/
+
+// Susan Tunkkari C++ 2021 NITS19K 
+
+#include <iostream>
+using namespace std;
+
+int gcd(int, int);
+
+int main() {
+	
+    int x, y;
+
+    cout << "Find the greatest common divisor of two numbers - Recursively"<< endl;
+    cout << "***********************************************************"<< endl;
+	
+	// get numbers from user
+    cout << "Enter x and y: ";
+    cin >> x >> y;
+
+    // For this algorithm, x must be larger than y.
+    if (x < y) 
+	{
+        int tmp = x;
+        x = y;
+        y = tmp;
+    }
+    
+	cout<< "GCD is: " <<gcd(x,y);
+	
+    return 0;
+}
+// recursive greatest common divisor
+int gcd(int x, int y) {
+	
+    if (y == 0) 
+	{
+        return x;
+    } 
+	else 
+	{
+        return gcd(y, x % y);
+    }
+}
